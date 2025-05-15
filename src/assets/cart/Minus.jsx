0095c0f97a46f7ef8f9d9caf8145f9bfec44dbe1
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-05-16 00:47:28 +0200

    Fix: icons

---
+++ src/assets/cart/Minus.jsx
@@ -0,0 +1,21 @@
+
+
+const Minus = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#8B8B8B"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M5 12h14"
+    />
+  </svg>
+)
+export default Minus
+
