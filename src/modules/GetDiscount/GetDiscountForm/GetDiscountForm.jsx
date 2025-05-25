585e6521f453dc7ef2a818f33989af6013aee4ae
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-05-25 20:58:29 +0200

    changing in GetDiscountForm

--- src/modules/GetDiscount/GetDiscountForm/GetDiscountForm.jsx
+++ src/modules/GetDiscount/GetDiscountForm/GetDiscountForm.jsx
@@ -7,7 +7,7 @@ import { getSaleApi } from "../../../shared/api/category-api"
 import styles from './GetDiscountForm.module.css'
 
 const GetDiscountForm = () => {
-    const [saleStatus, setSeleStatus] = useState(null);
+    const [saleStatus, setSaleStatus] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
@@ -19,7 +19,7 @@ const GetDiscountForm = () => {
         const { data, error } = await getSaleApi({ ...values });
         setLoading(false);
         if (data) {
-            setSeleStatus(data.status);
+            setSaleStatus(data.status);
             reset();
         } else {
             setError(error?.response?.data?.message || "Request failed")
@@ -31,14 +31,14 @@ const GetDiscountForm = () => {
             <form onSubmit={handleSubmit(onSubmit)} className={styles.wrapper}>
                 <input {...register("userName", { required: "Name is required" })} className={styles.inputStyle} type="text" placeholder="Name" />
                 {errors.userName && <p className={styles.error}>{errors.userName.message}</p>}
-                <input {...register("userPhone", { required: "Phpne number is required" })} className={styles.inputStyle} type="tel" placeholder="Phone number" />
+                <input {...register("userPhone", { required: "Phone number is required" })} className={styles.inputStyle} type="tel" placeholder="Phone number" />
                 {errors.userPhone && <p className={styles.error}>{errors.userPhone.message}</p>}
                 <input {...register("userEmail", { required: "Email is required" })} className={styles.inputStyle} type="email" placeholder="Email" />
                 {errors.userEmail && <p className={styles.error}>{errors.userEmail.message}</p>}
                 {error && <p className={styles.error}>{error}</p>}
                 {loading && <p>Loading...</p>}
                 <div className={styles.btn}>
-                    <button className={saleStatus === "OK" ? styles.btnActiveStyle : styles.btnStyle}  >{saleStatus === "OK" ? "Request Submitted" : "Get a discount"}</button>
+                    <button type="submit" className={saleStatus === "OK" ? styles.btnActiveStyle : styles.btnStyle}  >{saleStatus === "OK" ? "Request Submitted" : "Get a discount"}</button>
                 </div>
             </form>
         </>
